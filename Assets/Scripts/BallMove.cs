9ff6a3b5f19be03fdb8e52efbe900ae0fd3fa833
Billy Hilgert <billy.hilgert@CV-F11-60780.Student.CSD509J.Net>
2016-02-03 18:43:26 -0800

    Initial Upload

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMove : MonoBehaviour {
+
+	public Transform cam;
+	public Vector3 offset;
+	public float maxForce = 10f;
+	public float lerpConst = 0.2f;
+
+	Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+		//Move ball
+		Vector3 force = Vector3.zero;
+		force += Vector3.right * Input.GetAxis("Horizontal");
+		force += Vector3.forward * Input.GetAxis("Vertical");
+		force *= maxForce;
+		rb.AddForce(force);
+
+		//Move camera
+		Vector3 targetPos = transform.position;
+		targetPos += offset;
+
+		cam.position = Vector3.Lerp(cam.position, targetPos, lerpConst);
+    }
+}
---
+++ Assets/Scripts/DefensePlacement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class DefensePlacement : MonoBehaviour {
+
+	public Transform[] choiceA;
+	public Transform[] choiceB;
+
+	public Transform[] redMounts;
+	public Transform[] blueMounts;
+
+	void Start () {
+		if (choiceA.Length != 4 ||
+			choiceB.Length != 4 ||
+			redMounts.Length != 4 ||
+			blueMounts.Length != 4) {
+			Debug.LogError("Not all slots filled");
+			return;
+		}
+
+		List<Transform> redDefenses = new List<Transform>();
+		for (int i = 0; i < 4; i++) {
+			Transform nextDefense;
+			if (Random.value < 0.5)
+				nextDefense = choiceA[i];
+			else
+				nextDefense = choiceB[i];
+
+			redDefenses.Add(nextDefense);
+		}
+		redDefenses.OrderBy(u => Random.value);
+		for (int i = 0; i < 4; i++) {
+			Transform spawnedDefense = Instantiate(redDefenses[i]);
+			spawnedDefense.parent = redMounts[i];
+			spawnedDefense.localPosition = Vector3.zero;
+			spawnedDefense.localRotation = Quaternion.identity;
+		}
+
+		List<Transform> blueDefenses = new List<Transform>();
+		for (int i = 0; i < 4; i++) {
+			Transform nextDefense;
+			if (Random.value < 0.5)
+				nextDefense = choiceA[i];
+			else
+				nextDefense = choiceB[i];
+
+			blueDefenses.Add(nextDefense);
+		}
+		blueDefenses.OrderBy(u => Random.value);
+		for (int i = 0; i < 4; i++) {
+			Transform spawnedDefense = Instantiate(blueDefenses[i]);
+			spawnedDefense.parent = blueMounts[i];
+			spawnedDefense.localPosition = Vector3.zero;
+			spawnedDefense.localRotation = Quaternion.identity;
+		}
+	}
+}
